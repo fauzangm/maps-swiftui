37f3996679a520c35e4f9b735665a4a154da88da
Fauzan Ghozi Mubarak <fauzanghozi56@gmail.com>
2023-11-03 09:00:51 +0700

    updat marker

--- grocies-app/Views/LocationMapNotationComponent.swift
+++ grocies-app/Views/LocationMapNotationComponent.swift
@@ -25,13 +25,13 @@ struct LocationMapNotationComponent: View {
             Image(systemName: "triangle.fill")
                 .resizable()
                 .scaledToFit()
-                .frame(width: 30,height: 30)
-                .font(.headline)
-                .foregroundColor(.white)
-                .padding(6)
-                .background(accentColor)
-                .cornerRadius(36)
+                .foregroundColor(accentColor)
+                .frame(width: 10,height: 10)
+                .rotationEffect(Angle(degrees: 180))
+                .offset(y: -3)
+                .padding(.bottom,40)
         }
+        
     }
 }
 
--- grocies-app/Views/LocationsView.swift
+++ grocies-app/Views/LocationsView.swift
@@ -12,31 +12,15 @@ struct LocationsView: View {
     @EnvironmentObject private var vm : LocationViewModel
     var body: some View {
         ZStack{
-            Map(coordinateRegion: $vm.mapRegion)
+            mapLayer
                 .ignoresSafeArea()
             
             VStack(spacing: 0){
-         
                 header
                     .padding()
                 Spacer()
-                
-                ZStack{
-                    ForEach(vm.location) { location in
-                        if vm.mapLocation == location{
-                            LocationPreviewView(location: location)
-                                .shadow(color: Color.black.opacity(0.3), radius: 20)
-                                .padding()
-                                .transition(.asymmetric(insertion: .move(edge: .trailing), removal: .move(edge: .leading)))
-                        }
-                     
-                    }
-                }
-      
+                locationPreviewStack
             }
-   
-            
- 
         }
     }
 }
@@ -74,5 +58,32 @@ extension LocationsView {
         .background(.thickMaterial)
         .cornerRadius(10)
         .shadow(color: Color.black.opacity(0.3), radius: 20,x: 0,y: 15)
+    }
+    
+    private var mapLayer : some View{
+        Map(coordinateRegion: $vm.mapRegion, annotationItems: vm.location, annotationContent: { location in
+            MapAnnotation(coordinate: location.coordinates){
+                LocationMapNotationComponent()
+                    .scaleEffect(vm.mapLocation == location ? 1 : 0.7)
+                    .shadow(radius: 10)
+                    .onTapGesture {
+                        vm.showNextLocation(location: location)
+                    }
+            }
+        })
+    }
+    
+    private var locationPreviewStack : some View{
+        ZStack{
+            ForEach(vm.location) { location in
+                if vm.mapLocation == location{
+                    LocationPreviewView(location: location)
+                        .shadow(color: Color.black.opacity(0.3), radius: 20)
+                        .padding()
+                        .transition(.asymmetric(insertion: .move(edge: .trailing), removal: .move(edge: .leading)))
+                }
+             
+            }
+        }
     }
 }
